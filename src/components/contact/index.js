89252fed97997572d2c8fd5f9beb7a7b038789ac
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2024-04-04 10:15:25 +0300

    update

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -7,9 +7,11 @@ const Contact = () => {
     return (
         <div className="container">
             <section className="contact">
-                <h1 style={{ color: "#1677ff" }}>Get in touch!</h1>
                 <div>
-                    <Button type="primary" size="large" href="https://github.com/musclepassword"> <GithubOutlined />Github</Button>
+                <h1 style={{ color: "#1677ff" }}>Get in touch!</h1>
+                </div>
+                <div>
+                    <Button style={{ background: "#000" }} type="primary" size="large" href="https://github.com/musclepassword"> <GithubOutlined />Github</Button>
                 </div>
             </section>
         </div>
