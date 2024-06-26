de477e1bf8607145b63b20859d8713e90675ea91
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2024-06-26 14:33:19 +0300

    update

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -8,19 +8,23 @@ const Contact = () => {
     const { i18n } = useTranslation();
 
     return (
-        <div className="container">
-            <section className="contact">
-                <h1>{i18n.t('google_chrome_extension')}</h1>
-                <p>{i18n.t('google_chrome_extension_desc')}</p>
-                <img src={logo} alt="chrome_image" />
+        <>
+            <section className="chrome_extension">
+                <div className="container">
+                    <h1>{i18n.t('google_chrome_extension')}</h1>
+                    <p>{i18n.t('google_chrome_extension_desc')}</p>
+                    <img src={logo} alt="chrome_image" />
+                </div>
             </section>
-            <section className="contact">
-                <h1>{i18n.t('contact_for_support')}</h1>
-                <p>{i18n.t('contact_description')}</p>
-                <Button className="button-primary" type="primary" size="large" href="https://github.com/musclepassword" target="_blank"> <GithubOutlined />Github</Button>
-                <Button className="button-primary" size="large" href="mailto:cuneydbolukogluu@gmail.com" target="_blank"> <EditOutlined />  Email</Button>
-            </section>
-        </div>
+            <div className="container">
+                <section className="contact">
+                    <h1>{i18n.t('contact_for_support')}</h1>
+                    <p>{i18n.t('contact_description')}</p>
+                    <Button className="button-primary" type="primary" size="large" href="https://github.com/musclepassword" target="_blank"> <GithubOutlined />Github</Button>
+                    <Button className="button-primary" size="large" href="mailto:cuneydbolukogluu@gmail.com" target="_blank"> <EditOutlined />  Email</Button>
+                </section>
+            </div>
+        </>
     );
 }
 
