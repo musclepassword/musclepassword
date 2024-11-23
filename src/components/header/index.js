b9759d3aae2d65dd1fea2ac1c1bb0f58ec7aee32
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2024-11-23 16:19:21 +0300

    css and translation edited

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,25 @@
 import SelectLang from "../SelectLang";
 import Logo from "../../assets/image/musclepassword-logo.png"
+import { ChromeOutlined } from '@ant-design/icons';
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
+    const { i18n } = useTranslation();
 
     return (
         <div className="container">
             <header>
                 <div className="logo">
-                    <img src={Logo} alt="logo" /><h1><b>Muscle</b> <span>Password</span></h1>
+                    <img src={Logo} alt="logo" />
+                    <span>
+                        <h1><span>MUSCLE</span><br /> PASSWORD</h1>
+                    </span>
                 </div>
-                <SelectLang />
+                <div>
+                    <a href={'https://chromewebstore.google.com/detail/muscle-password/iimjcckbcjoehbfekigjpnlgmjllmgdk'} target="_blank">
+                    {i18n.t('add_to_chrome')} <ChromeOutlined />{' '}</a>
+                    <SelectLang />
+                </div>
             </header>
         </div>
     );
