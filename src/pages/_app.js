357af48ff419c1e31f0b5ea22a264196ad67f756
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2024-12-04 17:54:31 +0300

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
 import '@/styles/globals.scss';
 import 'antd/dist/reset.css';
-import nextI18nextConfig from '../../next-i18next.config';
 import { appWithTranslation } from 'next-i18next';
-import '../../i18n';
+import nextI18nextConfig from '../../next-i18next.config';
+import '../i18n';
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default appWithTranslation(App, nextI18nextConfig);
+export default appWithTranslation(App);
