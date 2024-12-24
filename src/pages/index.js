f67ae1f8cdb8708ce39ef6ca444f8341afd3fdf3
Yasin KALKAN <yasinburakkalkan@gmail.com>
2024-12-24 12:41:41 +0300

    [fix] Build errors fixed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,25 @@
 import Head from "next/head";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 // import styles from "@/styles/Home.module.css";
-import HeaderComponent from "@/components/Header";
+import HeaderComponent from "@/components/header";
 import HomeSection from "@/components/HomeSection";
-import { ConfigProvider } from 'antd';
+import { ConfigProvider } from "antd";
 import AboutSection from "@/components/About";
 import FAQSection from "@/components/FAQ";
 import ChromeExtenstionSection from "@/components/ChromeExtenstion";
 import ContactSection from "@/components/Contact";
 import FooterComponent from "@/components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export default function Home() {
   return (
@@ -38,14 +38,14 @@ export default function Home() {
         theme={{
           // algorithm: [darkAlgorithm, defaultAlgorithm],
           token: {
-            colorPrimary: '#53baf9',
+            colorPrimary: "#53baf9",
             // borderRadius: 7,
           },
           components: {
             // Button: {
             //     colorPrimary: 'red',
             //     algorithm: true,
-            // },                   
+            // },
           },
         }}
       >
