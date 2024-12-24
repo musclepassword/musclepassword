14e91cf969f35a426d6165b7c9fafb4f6540d963
Yasin KALKAN <yasinburakkalkan@gmail.com>
2024-12-24 12:44:41 +0300

    [fix] Build errors fixed.

--- src/components/HomeSection/index.js
+++ src/components/HomeSection/index.js
@@ -1,18 +1,20 @@
-import { useTranslation } from 'react-i18next';
-import GeneratePassword from '../GeneratePassword';
+import { useTranslation } from "react-i18next";
+import GeneratePassword from "../generatepassword";
 
 export default function HomeSection() {
-    const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
 
-    return (
-        <section className="home-section">
-            <div className="container">
-                <article>
-                    <h2>{t("Strong Random Password Generator")}</h2>
-                    <h3>{t("Generate strong passwords using random password generator")}</h3>
-                </article>
-                <GeneratePassword />
-            </div>
-        </section>
-    );
+  return (
+    <section className="home-section">
+      <div className="container">
+        <article>
+          <h2>{t("Strong Random Password Generator")}</h2>
+          <h3>
+            {t("Generate strong passwords using random password generator")}
+          </h3>
+        </article>
+        <GeneratePassword />
+      </div>
+    </section>
+  );
 }
