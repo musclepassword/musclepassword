29c30ab094263f7cfa9adf639644f9e54545b978
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2024-12-25 12:21:04 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-// import localFont from "next/font/local";
-// import styles from "@/styles/Home.module.css";
 import HeaderComponent from "@/components/header";
 import HomeSection from "@/components/HomeSection";
 import { ConfigProvider } from "antd";
@@ -9,17 +7,6 @@ import FAQSection from "@/components/FAQ";
 import ChromeExtenstionSection from "@/components/ChromeExtenstion";
 import ContactSection from "@/components/Contact";
 import FooterComponent from "@/components/Footer";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export default function Home() {
   return (
@@ -30,15 +17,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      > */}
-      {/* <main className={styles.main}> */}
       <ConfigProvider
         theme={{
-          // algorithm: [darkAlgorithm, defaultAlgorithm],
           token: {
             colorPrimary: "#53baf9",
+            colorLink: "#53baf9",
             // borderRadius: 7,
           },
           components: {
